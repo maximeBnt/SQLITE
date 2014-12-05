823c5f7132f9bab3f0c9ebb6d0d617408f2db5f8
maximeBnt <maxieevbm@gmail.com>
2014-12-05 17:24:11 +0100

    debut bdd

---
+++ app/src/main/java/com/example/binet/sqlite/Livre.java
@@ -0,0 +1,46 @@
+package com.example.binet.sqlite;
+
+/**
+ * Created by binet on 05/12/2014.
+ */
+public class Livre {
+
+    private int id;
+    private String isbn;
+    private String titre;
+
+    public Livre(){}
+
+    public Livre(String isbn, String titre){
+        this.isbn = isbn;
+        this.titre = titre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public String toString(){
+        return "ID : "+id+"\nISBN : "+isbn+"\nTitre : "+titre;
+    }
+}
